17cd03a6565166ba879d12f812f0f116d1b14015
Sergey Opolko <opolkosergey@gmail.com>
2015-10-18 15:18:16 +0300

    Task4 complete

---
+++ Task4.Tests/MathMethodsTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4.Tests
+{
+    [TestFixture]
+    class MathMethodsTests
+    {
+        [TestCase(null, 0, 4, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 4, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 0, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 1071, 462, Result = 21)]
+        [TestCase(null, -1071, 462, Result = 21)]
+        [TestCase(null, 1071, -462, Result = 21)]
+        [TestCase(null, -1071, -462, Result = 21)]
+        [TestCase(null, -462, -1071, Result = 21)]
+        [TestCase(null, -462, 1071, Result = 21)]
+        
+        public int GcdByEvklidMethod__ValueReturned(out long ticks, int a, int b)
+        {
+            return MathMethods.GcdByEvklidMethod(out ticks, a, b);
+        }
+
+        [TestCase(null, 0, 4, 146, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 4, 0, -146, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 0, 0, 146, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 1071, 462, 147, Result = 21)]
+        [TestCase(null, -1071, 462, -147, Result = 21)]
+        [TestCase(null, 1071, -462, 147, Result = 21)]
+        [TestCase(null, -1071, -462, -147, Result = 21)]
+
+        public int GcdByEvklidMethod__ValueReturned(out long ticks, int a, int b, int c)
+        {
+            return MathMethods.GcdByEvklidMethod(out ticks, a, b, c);
+        }
+
+        [TestCase(null, 2, 4, 144, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 4, 0, -146 ,24, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 6, 26, 146, 0, ExpectedException = typeof(ArgumentException))]
+        [TestCase(null, 1071, 462, 147,63, Result = 21)]
+        [TestCase(null, 63, 147, 462,1071, Result = 21)]
+        [TestCase(null, -1071,63,7, 462,-7, -147, Result = 7)]
+        [TestCase(null, 1071, -462, 147, -63, Result = 21)]
+        [TestCase(null, -63, -1071,-42, -462, -147, Result = 21)]
+
+        public int GcdByEvklidMethod__ValueReturned(out long ticks, params int[] values)
+        {
+            return MathMethods.GcdByEvklidMethod(out ticks, values);
+        }
+
+
+        [TestCase(null, 1071, 462, Result = 21)]
+        [TestCase(null, -1071, 462, Result = 21)]
+        [TestCase(null, 1071, -462, Result = 21)]
+        [TestCase(null, -1071, -462, Result = 21)]
+        [TestCase(null, -462, -1071, Result = 21)]
+        [TestCase(null, -462, 1071, Result = 21)]
+        
+        public int GcdBySteinMethod__ValueReturned(out long ticks, int a, int b)
+        {
+            return MathMethods.GcdBySteinMethod(out ticks, a, b);
+        }
+
+        [TestCase(null, 1071, 462, 147, Result = 21)]
+        [TestCase(null, -1071, 462, -147, Result = 21)]
+        [TestCase(null, 1071, -462, 147, Result = 21)]
+        [TestCase(null, -1071, -462, -147, Result = 21)]
+        public int GcdBySteinMethod__ValueReturned(out long ticks, int a, int b, int c)
+        {
+            return MathMethods.GcdBySteinMethod(out ticks, a, b,c);
+        }
+
+        [TestCase(null, 1071, 462, 147,63, Result = 21)]
+        [TestCase(null, 63, 147, 462,1071, Result = 21)]
+        [TestCase(null, -1071,63,7, 462,-7, -147, Result = 7)]
+        [TestCase(null, 1071, -462, 147, -63, Result = 21)]
+        [TestCase(null, -63, -1071,-42, -462, -147, Result = 21)]
+        public int GcdBySteinMethod__ValueReturned(out long ticks, params int[] values)
+        {
+            return MathMethods.GcdBySteinMethod(out ticks, values);
+        }
+    }
+}
--- Task4/MathMethods.cs
+++ Task4/MathMethods.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 
+
 namespace Task4
 {
     public static class MathMethods
@@ -64,9 +65,76 @@ namespace Task4
             return result;
         }
 
+        public static int GcdBySteinMethod(out long ticks, int a, int b)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var result = BinaryGcd(a, b);
+            watch.Stop();
+            ticks = watch.ElapsedTicks;
+            return result;
+        }
 
+        public static int GcdBySteinMethod(out long ticks, int a, int b, int c)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var result = BinaryGcd(a, b, c);
+            watch.Stop();
+            ticks = watch.ElapsedTicks;
+            return result;
+        }
+
+        public static int GcdBySteinMethod(out long ticks, params int[] valuesInts)
+        {
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            var result = BinaryGcd(valuesInts);
+            watch.Stop();
+            ticks = watch.ElapsedTicks;
+            return result;
+        }
 
         #region Private methods
+        private static int BinaryGcd(int a, int b)
+        {
+            if (a < 0) a *= -1;
+            if (b < 0) b *= -1;
+
+            if (a == 0) return b;
+            if (b == 0) return a;
+            if (a == b) return a;
+
+            if (a == 1) return 1;
+            if (b == 1) return 1;
+
+            if (a % 2 == 0 && b % 2 == 0)
+                return 2 * BinaryGcd(a / 2, b / 2);
+
+            if (a % 2 == 0 && b % 2 == 1)
+                return BinaryGcd(a / 2, b);
+
+            if (a % 2 == 1 && b % 2 == 0)
+                return BinaryGcd(a, b / 2);
+
+            if (a % 2 == 1 && b % 2 == 1 && b > a)
+                return BinaryGcd((b - a) / 2, a);
+
+            return BinaryGcd((a - b) / 2, b);
+        }
+
+        private static int BinaryGcd(params int[] valuesInts)
+        {
+            int gcd = BinaryGcd(valuesInts[0], valuesInts[1]);
+
+            for (int i = 2; i < valuesInts.Length; i++)
+            {
+                gcd = BinaryGcd(valuesInts[i], gcd);
+            }
+
+            return Math.Abs(gcd);
+        }
+
         private static int Gcd(int a, int b)
         {
             if (a == 0 || b == 0)
@@ -91,7 +159,7 @@ namespace Task4
         }
 
         private static int Gcd(params int[] valuesInts)
-        { 
+        {
             int gcd = Gcd(valuesInts[0], valuesInts[1]);
 
             for (int i = 2; i < valuesInts.Length; i++)
